3ab4604001d154937c13443c7b3fad6e316a3bf9
anuragdaksh7 <anuragdaksh77777@gmail.com>
2023-12-27 21:43:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Live Cursor
+
+get position of cursor of all the users on the page in real time.
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+const user = Object();
+
+io.on('connection', (socket) => {
+    // socket.broadcast.emit("hi");
+    console.log('a user connected');
+    socket.on("chat message", (msg) => {
+        io.emit("chat message",msg);
+    });
+    socket.on("mouseUpdate", (msg) => {
+        // console.log(msg);
+        user[msg[0]] = [msg[1],msg[2]];
+        io.emit("mouseUpdate",user);
+    });
+    socket.on("unload",(seed)=>{
+        console.log("unloading user "+seed);
+        user[seed] = undefined;
+    })
+    socket.on('disconnect', () => {
+        // user[seed] = null;
+        console.log('user disconnected');
+    });
+})
+
+server.listen(5000, () => {
+    console.log("listening on port 5000");
+});
